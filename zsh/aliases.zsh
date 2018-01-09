5776e74392e94c015272402aba6672ae0f42a496
lainiwa <kirrik96@gmail.com>
2018-01-09 18:56:23 +0300

    grc now doesnt through exception if command not found. also added aliases for unbuffer and TRN alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,26 +10,34 @@ alias lal='ls -Al'
 if [[ -n $(which grc) ]]; then
     for cmd in $(echo "configure diff make gcc ld netstat ping ping6 traceroute traceroute6 head tail dig mount ps mtr df ifconfig")
     do
-        alias $cmd="grc --colour=auto $cmd"
+        alias $cmd="__grc_color__ $cmd"
     done
 fi
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-#alias ..='cd ..'
-#alias ...='cd ../..'
-#alias ....='cd ../../..'
 alias mkdir='mkdir -pv'
 alias tree='tree -C'
+alias U='unbuffer '
 
 alias -g H="| head"
 alias -g T="| tail"
 alias -g G="| grep"
 alias -g L="| less"
+alias -g C='| column -t'
 alias -g NE="2> /dev/null"
 alias -g NUL="> /dev/null 2>&1"
-alias -g C='| column -t'
 
-mkcd() {
-    mkdir -- "$1" && cd -P -- "$1"
+mkcd() { mkdir -- "$1" && cd -P -- "$1" ; }
+
+# example: echo $PATH TRN :
+__rt__() { tr "$2" "$1" ; }
+alias -g TRN='| __rt__ "\n" '
+
+__grc_color__() {
+    if hash "$1" 2>/dev/null 1>&2 ; then
+        grc --colour=auto $@
+    else
+        $@
+    fi
 }
