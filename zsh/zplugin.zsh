6f20a1625e27c1071590cc528dbcea7240e326ad
lainiwa <kirrik96@gmail.com>
2018-05-22 04:22:46 +0300

    Script `my` assumed being in PATH. ZSH load speed increased. As Xorg now runs non-root, user-defined PATH can be used. Therefore script `my` is used again as being in PATH. Changed load to light to enable turbo load (see zplugin's wiki for details). Changed pip3 completion generation (2s lod --> 0.6s load). Add a config change according to changes in zsh-command-time plugin.

--- zsh/zplugin.zsh
+++ zsh/zplugin.zsh
@@ -7,39 +7,46 @@ source "$HOME/.zplugin/bin/zplugin.zsh"
 autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zplugin]=_zplugin
 
-zplugin ice from"gh-r" as"command";         zplugin load junegunn/fzf-bin
-zplugin ice as"command" pick"bin/fzf-tmux"; zplugin load junegunn/fzf
+zplugin ice from"gh-r" as"command";         zplugin light junegunn/fzf-bin
+zplugin ice as"command" pick"bin/fzf-tmux"; zplugin light junegunn/fzf
 zplugin snippet 'https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh'   # because no way(?) to src two files at once
 zplugin snippet 'https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh'
 
-# doesnt work anymore
-# zplugin snippet --command 'https://gist.githubusercontent.com/blueyed/54a257c411310a28805a/raw/cf9be9e0097839af88518e272c39256534799da0/_pip.zsh'
+# eval "$(pip3 completion --zsh)"  # <-- slow af. soo:
+function _pip_completion {
+  local words cword
+  read -Ac words
+  read -cn cword
+  reply=( $( COMP_WORDS="$words[*]" \
+             COMP_CWORD=$(( cword-1 )) \
+             PIP_AUTO_COMPLETE=1 $words[1] ) )
+}
 compctl -K _pip_completion pip3
-eval "$(pip3 completion --zsh)"
 
-zplugin load supercrabtree/k                        # ls -lh + git helpers
-zplugin load soimort/translate-shell
-zplugin load mafredri/zsh-async
-zplugin load seletskiy/zsh-fuzzy-search-and-edit    # Ctrl+P
-zplugin load zlsun/solarized-man
-zplugin load hcgraf/zsh-sudo                        # Toggles "sudo" before the current/previous command by pressing ESC-ESC.
-zplugin load MichaelAquilina/zsh-you-should-use
-zplugin load Tarrasch/zsh-command-not-found         # Guess what to install when running an unknown command.
-zplugin load Tarrasch/zsh-functional
-zplugin load Tarrasch/zsh-autoenv
-zplugin load tonyseek/oh-my-zsh-virtualenv-prompt   # I use virtualenv_prompt_info() from here.
-zplugin load bric3/nice-exit-code                   # Maps exit status code to human readable string.
-zplugin load sindresorhus/pretty-time-zsh           # Used in prompt to convert seconds to human-readable format.
-zplugin load popstas/zsh-command-time               # Print time after program finishes. I use it in right prompt.
-zplugin load rupa/z                                 # Looks like autojump
-zplugin load andrewferrier/fzf-z                    # alt+g to choose from most frecent folders
-zplugin load chrissicool/zsh-256color
-zplugin load zsh-users/zsh-completions              # Additional completion definitions for Zsh.
-zplugin load zsh-users/zsh-autosuggestions
-zplugin load zsh-users/zsh-syntax-highlighting      # Syntax highlighting bundle
-zplugin load zsh-users/zsh-history-substring-search # Crtl+R search now highlited
 
-zplugin ice pick"manydots-magic"; zplugin load knu/zsh-manydots-magic
+zplugin light supercrabtree/k                        # ls -lh + git helpers
+zplugin light soimort/translate-shell
+zplugin light mafredri/zsh-async
+zplugin light seletskiy/zsh-fuzzy-search-and-edit    # Ctrl+P
+zplugin light zlsun/solarized-man
+zplugin light hcgraf/zsh-sudo                        # Toggles "sudo" before the current/previous command by pressing ESC-ESC.
+zplugin light MichaelAquilina/zsh-you-should-use
+zplugin light Tarrasch/zsh-command-not-found         # Guess what to install when running an unknown command.
+zplugin light Tarrasch/zsh-functional
+zplugin light Tarrasch/zsh-autoenv
+zplugin light tonyseek/oh-my-zsh-virtualenv-prompt   # I use virtualenv_prompt_info() from here.
+zplugin light bric3/nice-exit-code                   # Maps exit status code to human readable string.
+zplugin light sindresorhus/pretty-time-zsh           # Used in prompt to convert seconds to human-readable format.
+zplugin light popstas/zsh-command-time               # Print time after program finishes. I use it in right prompt.
+zplugin light rupa/z                                 # Looks like autojump
+zplugin light andrewferrier/fzf-z                    # alt+g to choose from most frecent folders
+zplugin light chrissicool/zsh-256color
+zplugin light zsh-users/zsh-completions              # Additional completion definitions for Zsh.
+zplugin light zsh-users/zsh-autosuggestions          # slow!
+zplugin light zsh-users/zsh-syntax-highlighting      # Syntax highlighting bundle
+zplugin light zsh-users/zsh-history-substring-search # Crtl+R search now highlited
+
+zplugin ice pick"manydots-magic"; zplugin light knu/zsh-manydots-magic
 
 # tonyseek/oh-my-zsh-virtualenv-prompt
 PS1='%B%F{green}$(virtualenv_prompt_info)'$PS1
@@ -50,7 +57,7 @@ RPS1='%B%F{red}$(nice_exit_code)%f%b'
 # popstas/zsh-command-time
 # sindresorhus/pretty-time-zsh
 ZSH_COMMAND_TIME_MIN_SECONDS=1
-ZSH_COMMAND_TIME_ECHO=''
+ZSH_COMMAND_TIME_MSG=''
 RPS1=$RPS1' %B%F{green}$([[ -n $ZSH_COMMAND_TIME ]] && pretty-time $ZSH_COMMAND_TIME)%f%b'
 
 # mafredri/zsh-async
