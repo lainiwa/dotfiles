4d4ef7040acb82eca89fe709c6653dcf7cc57220
lainiwa <kirrik96@gmail.com>
2018-12-12 16:23:43 +0300

    Refactor ~/.zsh/zplugin.zsh

--- zsh/zplugin.zsh
+++ zsh/zplugin.zsh
@@ -1,10 +1,12 @@
 
 # Install `zplugin` if not installed
-if [ ! -d "$HOME/.zplugin" ]; then
+if [ ! -d "${HOME}/.zplugin" ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
 fi
 
-source "$HOME/.zplugin/bin/zplugin.zsh"
+
+# Load `zplugin`
+source "${HOME}/.zplugin/bin/zplugin.zsh"
 autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zplugin]=_zplugin
 
@@ -15,17 +17,24 @@ zplugin ice as"command" pick"bin/fzf-tmux"; zplugin light junegunn/fzf
 zplugin ice multisrc"shell/{completion,key-bindings}.zsh" id-as"fzf-zsh" pick"/dev/null";
                                             zplugin light junegunn/fzf
 
-zplugin ice as"command" pick"v";            zplugin light rupa/v
-zplugin ice as"command" pick"bin/git-dsf";  zplugin light zdharma/zsh-diff-so-fancy
-zplugin ice if'[[ -n "$commands[gawk]" ]]'; zplugin light soimort/translate-shell
 
-zplugin ice pick"manydots-magic"; zplugin light knu/zsh-manydots-magic
-
+# Install completions for `my` script and for python-gist
+# (use `-f` flag to force completion installation)
 zplugin ice as"completion" if"[ -f '${HOME}/.zsh/completions/_my' ]" id-as"my";
-    zplugin snippet "${HOME}/.zsh/completions/_my"  # flag `-f` for developing mode
+    zplugin snippet "${HOME}/.zsh/completions/_my"
 zplugin ice as"completion" if"[ -f '${HOME}/.local/share/gist/gist.zsh' ]" id-as"gist" mv"gist.zsh -> _gist";
     zplugin snippet "${HOME}/.local/share/gist/gist.zsh"
 
+
+# Install a number of plugins,
+# which require setting `ice` options
+zplugin ice as"command" pick"v";                zplugin light rupa/v
+zplugin ice as"command" pick"bin/git-dsf";      zplugin light zdharma/zsh-diff-so-fancy
+zplugin ice             pick"manydots-magic";   zplugin light knu/zsh-manydots-magic
+zplugin ice if'[[ -n "$commands[gawk]" ]]';     zplugin light soimort/translate-shell
+
+
+# Install all other plugins
 zplugin light supercrabtree/k                        # ls -lh + git helpers
 zplugin light mafredri/zsh-async
 zplugin light seletskiy/zsh-fuzzy-search-and-edit    # Ctrl+P
@@ -40,14 +49,16 @@ zplugin light andrewferrier/fzf-z                    # ctrl+g to choose from mos
 zplugin light chrissicool/zsh-256color
 zplugin light zsh-users/zsh-completions              # Additional completion definitions for Zsh.
 zplugin light zsh-users/zsh-autosuggestions          # slow!
-zplugin light zsh-users/zsh-syntax-highlighting      # Syntax highlighting bundle
-zplugin light zsh-users/zsh-history-substring-search # Crtl+R search now highlited
+zplugin light zsh-users/zsh-syntax-highlighting      # Syntax highlighting
+zplugin light zsh-users/zsh-history-substring-search # Crtl+R search highlited
 zplugin light changyuheng/fz                         # lets z+[Tab] and zz+[Tab]. Doesn't integrate well with autosuggestions
                                                      # but there is hope: https://github.com/changyuheng/fz/pull/15
 
 
+# Setting appropriate variables
+
 # tonyseek/oh-my-zsh-virtualenv-prompt
-PS1='%B%F{green}$(virtualenv_prompt_info)'$PS1
+PS1='%B%F{green}$(virtualenv_prompt_info)'${PS1}
 
 # bric3/nice-exit-code
 RPS1='%B%F{red}$(nice_exit_code)%f%b'
@@ -56,7 +67,7 @@ RPS1='%B%F{red}$(nice_exit_code)%f%b'
 # sindresorhus/pretty-time-zsh
 ZSH_COMMAND_TIME_MIN_SECONDS=1
 ZSH_COMMAND_TIME_MSG=''
-RPS1=$RPS1' %B%F{green}$([[ -n $ZSH_COMMAND_TIME ]] && pretty-time $ZSH_COMMAND_TIME)%f%b'
+RPS1=${RPS1}' %B%F{green}$([[ -n ${ZSH_COMMAND_TIME} ]] && pretty-time ${ZSH_COMMAND_TIME})%f%b'
 
 # mafredri/zsh-async
 # seletskiy/zsh-fuzzy-search-and-edit
@@ -66,9 +77,11 @@ export EDITOR="${EDITOR:-vim}"
 # zsh-users/zsh-syntax-highlighting
 export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 
-# Install completions
+
+# Load completions
 autoload -Uz compinit
 compinit -i  # if not `-i` spauns warning when 'sudo -s'
+
 
 # Execute compdefs provided by rest of plugins
 zplugin cdreplay -q
