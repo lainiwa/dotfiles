8d9e65246387c926ab24c9d83652fd2d7a4298fb
lainiwa <kirrik96@gmail.com>
2018-12-12 15:25:46 +0300

    Load python-gist completions

--- zsh/zplugin.zsh
+++ zsh/zplugin.zsh
@@ -21,6 +21,7 @@ zplugin ice if'[[ -n "$commands[gawk]" ]]'; zplugin light soimort/translate-shel
 # stoped working after zplugin update (?)
 # zplugin ice pick"manydots-magic"; zplugin light knu/zsh-manydots-magic
 
+zplugin ice as"completion" mv"gist.zsh -> _gist" id-as"gist"; zplugin snippet "${HOME}/.local/share/gist/gist.zsh"
 
 zplugin light supercrabtree/k                        # ls -lh + git helpers
 zplugin light mafredri/zsh-async
@@ -42,7 +43,7 @@ zplugin light changyuheng/fz                         # lets z+[Tab] and zz+[Tab]
                                                      # but there is hope: https://github.com/changyuheng/fz/pull/15
 
 # zplugin light lainiwa/pastebin  # experimenting with my extention
-zplugin ice as"completion"; zplugin snippet "${HOME}/.zsh/completions/_my"  # flag `-f` for developing mode
+zplugin ice as"completion" id-as"my"; zplugin snippet "${HOME}/.zsh/completions/_my"  # flag `-f` for developing mode
 
 # tonyseek/oh-my-zsh-virtualenv-prompt
 PS1='%B%F{green}$(virtualenv_prompt_info)'$PS1
