aefe8a5187e7f6422fa2ec2745768ae5a87c1b1a
lainiwa <kirrik96@gmail.com>
2019-07-31 10:35:26 +0300

    Add quick installation script

---
+++ quick_install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -
+set -o errexit   # exit on fail
+set -o nounset   # exit on undeclared variable
+set -o xtrace    # trace execution
+
+# Add github's host key to known_hosts (if not yet)
+if ! grep github.com ~/.ssh/known_hosts; then
+    ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+fi
+
+# Clone repository (if not yet)
+if [ ! -d ~/.dotfiles ]; then
+    # First try cloning via ssh (only works if owner of repo)
+    git clone     git@github.com:lainiwa/dotfiles.git ~/.dotfiles ||
+    # Clone via https otherwise
+    git clone https://github.com/lainiwa/dotfiles.git ~/.dotfiles
+fi
+
+# Execute installer
+~/.dotfiles/install
