098403aec0aef95ae8ce17737876e49b88352b42
lainiwa <kirrik96@gmail.com>
2020-03-03 01:47:55 +0300

    Change cgo.csm to fetch from git

--- guix-packages/cgo.scm
+++ guix-packages/cgo.scm
@@ -2,19 +2,21 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix build-system gnu)
-  #:use-module (guix download))
+  #:use-module (guix git-download))
 
 (define-public cgo
   (package
     (name "cgo")
     (version "0.6.0")
     (source (origin
-              (method url-fetch)
-              (uri (string-append "https://github.com/kieselsteini/cgo/archive/v" version
-                ".tar.gz"))
+              (method git-fetch)
+              (uri (git-reference
+                     (url "https://github.com/kieselsteini/cgo")
+                     (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
               (sha256
                (base32
-                "0xzn6xz0h3s1c8mnnn2mjd5px0j0l60v4d225dd0p7vn27whmpi5"))))
+                "016qslrkk3kk93q9x5mzgd8zdk91fd0h6vxwilzc2x3blamdicrc"))))
     (build-system gnu-build-system)
     (arguments
          '(#:phases (modify-phases %standard-phases (delete 'configure))
