7fe990d7cbc29053adbf38ec7a7f717ef3cfa698
lainiwa <kirrik96@gmail.com>
2020-03-14 04:18:29 +0300

    Rewrite antibody config for static loading

--- zsh/antibody.zsh
+++ zsh/antibody.zsh
@@ -1,46 +1,13 @@
 
-if [[ -z $commands[antibody] ]]; then
-    curl -sfL git.io/antibody | sh -s - -b ~/bin
+# Install antibody
+if (( ! ${+commands[antibody]} )); then
+    curl --silent --fail --location git.io/antibody | sh -s - -b ~/bin
 fi
 
-
-GEN_COMPLETION_DIR=${HOME}/.cache/generated_zsh_completions
-
-if [[ ! -d $GEN_COMPLETION_DIR ]]; then
-    mkdir --parents "${GEN_COMPLETION_DIR}"
-    pushd "${GEN_COMPLETION_DIR}"
-    GH=https://raw.githubusercontent.com
-
-
-    wget "${GH}/ogham/exa/master/contrib/completions.zsh" --output-file=logfile
-    wget "${GH}/jdowner/gist/alpha/share/gist.zsh" --output-file=_gist
-    wget "${GH}/pimutils/khal/master/misc/__khal" --output-file=_khal
-    wget "${GH}/jarun/Buku/master/auto-completion/zsh/_buku"
-    wget "${GH}/docker/compose/master/contrib/completion/zsh/_docker-compose"
-    wget "${GH}/beetbox/beets/master/extra/_beet"
-    wget 'https://git.savannah.gnu.org/cgit/guix.git/plain/etc/completion/zsh/_guix'
-
-    if [[ -z $commands[rclone] ]]; then
-        rclone genautocomplete zsh _rclone
-    fi
-    if [[ -z $commands[poetry] ]]; then
-        poetry completions zsh > _poetry
-    fi
-    if [[ -z $commands[rustup] ]]; then
-        rustup completions zsh cargo > _cargo
-        rustup completions zsh rustup > _rustup
-    fi
-
-    unset GH
-    popd
-fi
-
-fpath=(${GEN_COMPLETION_DIR} $fpath)
-fpath=(~/.zsh/completions/ $fpath)
-unset GEN_COMPLETION_DIR
-
-
-source <(antibody init)
+# Configuration
+ZSH_CACHE_DIR=${HOME}/.cache/zsh
+COMPLETIONS_DIR=${ZSH_CACHE_DIR}/completions
+SOURCEABLES_DIR=${ZSH_CACHE_DIR}/sourceables
 
 # Python virtual environment name
 AGKOZAK_CUSTOM_PROMPT='%B%F{green}$(virtualenv_prompt_info)'
@@ -50,7 +17,6 @@ AGKOZAK_CUSTOM_PROMPT+='%(!.%S%B.%B%F{yellow})%n%1v%(!.%b%s.%f%b) '
 AGKOZAK_CUSTOM_PROMPT+=$'%B%F{green}%2v%f%b '
 # Prompt character
 AGKOZAK_CUSTOM_PROMPT+='%B%F{red}%(4V.:.%#)%f%b '
-
 # Git status
 AGKOZAK_CUSTOM_RPROMPT='%(3V.%F{yellow}%3v%f.)'
 # Exit status
@@ -59,56 +25,74 @@ AGKOZAK_CUSTOM_RPROMPT+=' %B%F{red}$(nice_exit_code)%f%b'
 export ZSH_COMMAND_TIME_MIN_SECONDS=1
 export ZSH_COMMAND_TIME_MSG=''
 AGKOZAK_CUSTOM_RPROMPT+=' %B%F{green}$([[ -n ${ZSH_COMMAND_TIME} ]] && pretty-time ${ZSH_COMMAND_TIME})%f%b'
-
-
-export ZSHZ_DATA="${HOME}/.cache/.z"
-export ZSHZ_OWNER=$(basename "${HOME}")
-
+# Zsh-z configuration
+export ZSHZ_DATA=${HOME}/.cache/.z
+export ZSHZ_OWNER=${HOME:t}
+# Gitcd configuration
 export GITCD_HOME=${HOME}/tmp
 export GITCD_TRIM=1
 
-antibody bundle <<- EOF
-    # prompt
-    tonyseek/oh-my-zsh-virtualenv-prompt
-    bric3/nice-exit-code
-    sindresorhus/pretty-time-zsh
-    popstas/zsh-command-time
-    agkozak/agkozak-zsh-prompt
+#
+if [[ ! -d $COMPLETIONS_DIR ]]; then
+    # Create and enter completions directory
+    mkdir --parents -- "${COMPLETIONS_DIR}"
+    pushd "${COMPLETIONS_DIR}"
+    # Download completions
+    GH=https://raw.githubusercontent.com
+    wget --quiet "${GH}/ogham/exa/master/contrib/completions.zsh" --output-file=logfile
+    wget --quiet "${GH}/jdowner/gist/alpha/share/gist.zsh" --output-file=_gist
+    wget --quiet "${GH}/pimutils/khal/master/misc/__khal" --output-file=_khal
+    wget --quiet "${GH}/jarun/Buku/master/auto-completion/zsh/_buku"
+    wget --quiet "${GH}/docker/compose/master/contrib/completion/zsh/_docker-compose"
+    wget --quiet "${GH}/beetbox/beets/master/extra/_beet"
+    wget --quiet 'https://git.savannah.gnu.org/cgit/guix.git/plain/etc/completion/zsh/_guix'
+    unset GH
+    # Generate completions for present commands
+    if (( ${+commands[rclone]} )); then
+        rclone genautocomplete zsh _rclone
+    fi
+    if (( ${+commands[poetry]} )); then
+        poetry completions zsh > _poetry
+    fi
+    if (( ${+commands[rustup]} )); then
+        rustup completions zsh cargo > _cargo
+        rustup completions zsh rustup > _rustup
+    fi
+    # Leave completions directory
+    popd
+fi
 
-    agkozak/zsh-z
-    andrewferrier/fzf-z
+fpath+=(${COMPLETIONS_DIR})
+fpath+=(~/.zsh/completions/)
+export FPATH=${HOME}/.zsh/completions:${FPATH}
 
-    # Rename tmux pane to current folder's basename
-    trystan2k/zsh-tab-title
+if [[ ! -d $SOURCEABLES_DIR ]]; then
+    # Create and enter completions directory
+    mkdir --parents -- "${SOURCEABLES_DIR}"
+    pushd "${SOURCEABLES_DIR}"
+    # Generate completions for present commands
+    if (( ${+commands[antibody]} )); then
+        antibody bundle < ~/.zsh/plugins.txt > plugins.zsh
+    fi
+    if (( ${+commands[pyenv]} )); then
+        pyenv init - --no-rehash > pyenv.zsh
+    fi
+    if (( ${+commands[dircolors]} )); then
+        dircolors --bourne-shell \
+            <(curl --silent 'https://raw.githubusercontent.com/trapd00r/LS_COLORS/master/LS_COLORS') \
+            >lscolors.sh
+    fi
+    # Leave completions directory
+    popd
+fi
 
-    # Add command-line online translator
-    soimort/translate-shell
+for sourceable in "${SOURCEABLES_DIR}"/*; do
+    source "${sourceable}"
+done
 
-    # Toggles "sudo" before the current/previous command by pressing ESC-ESC.
-    hcgraf/zsh-sudo
-
-    # Run fg command to return to foregrounded (Ctrl+Z'd) vim
-    mdumitru/fancy-ctrl-z
-
-    lainiwa/gitcd
-
-    # Adds git open
-    paulirish/git-open
-
-    bobthecow/git-flow-completion
-    spwhitt/nix-zsh-completions
-
-    # Get gitignore template with gi command
-    #voronkovich/gitignore.plugin.zsh
-
-    zsh-users/zsh-completions
-    robobenklein/zdharma-history-search-multi-word
-    zsh-users/zsh-autosuggestions
-    zsh-users/zsh-syntax-highlighting
-
-    ankaan/zsh-manydots-magic
-
-EOF
+unset ZSH_CACHE_DIR
+unset COMPLETIONS_DIR
+unset SOURCEABLES_DIR
 
 autoload -U compinit
 compinit
