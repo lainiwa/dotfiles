06e3e19361d7e83481b2840d8aa2e7576ad85331
lainiwa <kirrik96@gmail.com>
2020-05-01 09:00:04 +0300

    Add a few home.packages to home.nix

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -13,4 +13,10 @@
   # the Home Manager release notes for a list of state version
   # changes in each release.
   home.stateVersion = "20.03";
+
+  home.packages = with pkgs; [
+    ffsend
+    bat
+    exa
+  ];
 }
