0bb597fc297538211901eeaecc1a899558c50c49
lainiwa <kirrik96@gmail.com>
2020-12-27 19:32:46 +0300

    Drop mpv-stats

--- config/mpv/modules.lua
+++ config/mpv/modules.lua
@@ -1,4 +1,4 @@
 local mpv_scripts_dir_path = os.getenv("HOME") ..  "/.config/mpv/scripts/"
 function load(relative_path) dofile(mpv_scripts_dir_path .. relative_path) end
+-- load("mpv-stats/stats.lua")
 load("mpvSockets/mpvSockets.lua")
-
