6727de0c35469a7f61e4d3147fc9e22fcc0a2826
lainiwa <kirrik96@gmail.com>
2021-04-27 17:09:18 +0300

    Add mpv_sponsorblock

--- config/mpv/scripts/modules.lua
+++ config/mpv/scripts/modules.lua
@@ -2,3 +2,4 @@ local mpv_scripts_dir_path = os.getenv("HOME") ..  "/.config/mpv/scripts/"
 function load(relative_path) dofile(mpv_scripts_dir_path .. relative_path) end
 -- load("mpv-stats/stats.lua")
 load("mpvSockets/mpvSockets.lua")
+load("mpv_sponsorblock/sponsorblock.lua")
